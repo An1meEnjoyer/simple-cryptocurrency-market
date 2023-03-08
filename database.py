4b78ae672df1a24f76aa6ec5da5bb17587980904
Max <I'l_add_it@later.mb>
2023-03-08 14:37:42 +0300

    add sell and buy funcs to database.py

--- database.py
+++ database.py
@@ -17,6 +17,66 @@ class Connect():
     
     def add_user(self, username:str, password:str) -> None:
         self.cursor.execute("INSERT INTO users (username, password) VALUES(%s, %s);", (username, password))
+       
+       
+    def sell(self, user_id:int, first_crupto_id:int, second_crupto_id:int, price:float, value:float) -> None:
+        # get aviable crypto balance
+        self.cursor.execute("SELECT value FROM balance WHERE user_id = %s AND crypto_id = %s;", (user_id, first_crupto_id))
+        first_crypto_balance_before = self.cursor.fetchone()[0]
+        
+        if first_crypto_balance_before >= value:
+            first_crypto_balance_after = first_crypto_balance_before - value
+            
+            self.cursor.execute("SELECT value FROM balance WHERE user_id = %s AND crypto_id = %s;", (user_id, second_crupto_id))
+            second_crypto_balance_before = self.cursor.fetchone()
+            
+            second_crypto_balance_after = value*price
+            if second_crypto_balance_before:
+                second_crypto_balance_after += second_crypto_balance_before[0]
+            else:
+                self.cursor.execute("INSERT INTO balance (user_id, crypto_id, value) VALUES (%s, %s, 0);", (user_id, second_crupto_id))
+            
+            self.cursor.execute(""" UPDATE balance
+                                    SET value = %s
+                                    WHERE user_id = %s AND crypto_id = %s;""",
+                                    (first_crypto_balance_after, user_id, first_crupto_id))
+            
+            self.cursor.execute(""" UPDATE balance
+                                    SET value = %s
+                                    WHERE user_id = %s AND crypto_id = %s;""",
+                                    (second_crypto_balance_after, user_id, second_crupto_id))
+            
+            self.save_changes()
+            
+       
+    def buy(self, user_id:int, first_crupto_id:int, second_crupto_id:int, price:float, value:float) -> None:
+        # get aviable crypto balance
+        self.cursor.execute("SELECT value FROM balance WHERE user_id = %s AND crypto_id = %s;", (user_id, second_crupto_id))
+        second_crypto_balance_before = self.cursor.fetchone()[0]
+        
+        if second_crypto_balance_before >= value*price:
+            second_crypto_balance_after = second_crypto_balance_before - value*price
+            
+            self.cursor.execute("SELECT value FROM balance WHERE user_id = %s AND crypto_id = %s;", (user_id, first_crupto_id))
+            first_crypto_balance_before = self.cursor.fetchone()
+            
+            first_crypto_balance_after = value
+            if first_crypto_balance_before:
+                first_crypto_balance_after += first_crypto_balance_before[0]
+            else:
+                self.cursor.execute("INSERT INTO balance (user_id, crypto_id, value) VALUES (%s, %s, 0);", (user_id, first_crupto_id))
+            
+            self.cursor.execute(""" UPDATE balance
+                                    SET value = %s
+                                    WHERE user_id = %s AND crypto_id = %s;""",
+                                    (first_crypto_balance_after, user_id, first_crupto_id))
+            
+            self.cursor.execute(""" UPDATE balance
+                                    SET value = %s
+                                    WHERE user_id = %s AND crypto_id = %s;""",
+                                    (second_crypto_balance_after, user_id, second_crupto_id))
+            
+            self.save_changes()
             
             
     def save_changes(self) -> None:
