d9c544f0c58f9d74ff9b1cc7b487c00d8554b0ce
Max <I'l_add_it@later.mb>
2023-03-20 21:07:39 +0300

    rebuild more comfortable structure and comments in api

--- api.py
+++ api.py
@@ -9,13 +9,21 @@ class Connect():
         self.conn = psycopg2.connect(**settings)
         self.cursor = self.conn.cursor()
        
-        
+    
+    # - - - - - - - - - - - - - - - - - - GENERAL FUNCS - - - - - - - - - - - - - - - - - - - - - - - - - - -
     def get_data(self, table:str) -> list:
         # select all lines from table
         self.cursor.execute("SELECT * FROM %s;" % table)
         return self.cursor.fetchall()
     
-        
+    
+    def save_changes(self) -> None:
+        # just commit
+        self.conn.commit()
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    
+    
+    # - - - - - - - - - - - - - - - - - - USERS FUNCS - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     def find_user(self, user_name:str, password:str) -> dict:
         self.cursor.execute("SELECT id, superuser FROM users WHERE username = %s AND password = %s;", (user_name, password))
         sup = self.cursor.fetchone()
@@ -24,7 +32,24 @@ class Connect():
             return {'id': 0, 'superuser': 0}
         return {'id': sup[0], 'superuser': sup[1]}
     
-
+    
+    def add_user(self, username:str, password:str) -> None:
+        # register
+        self.cursor.execute("INSERT INTO users (username, password) VALUES(%s, %s);", (username, password))
+        self.save_changes()
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    
+    
+    # - - - - - - - - - - - - - - - - - - CRYPTO  FUNCS - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    def add_crypto(self, user_name:str, password:str, crypto_name:str, short_name:str) -> None:
+        # add new cryptocurrency (only for superuser)
+        if self.find_user(user_name, password)['superuser']:
+            self.cursor.execute("INSERT INTO cryptocurrencies (name, short_name) VALUES (%s, %s);", (crypto_name, short_name))
+            self.save_changes()
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    
+       
+    # - - - - - - - - - - - - - - - - - - BALANCE FUNCS - - - - - - - - - - - - - - - - - - - - - - - - - - -
     def get_bal(self, user_name:str, password:str) -> dict:
         user_id =  self.find_user(user_name, password)['id']
         if not user_id:
@@ -36,30 +61,19 @@ class Connect():
             pretty_bal[i[0].strip()] = float(i[1])
             
         return pretty_bal
-            
+    
 
-    def set_bal(self, user_name:str, password:str, crypto_id:str = 'USDT', value:float = 100):
+    def set_bal(self, user_name:str, password:str, crypto_id:str = 'USDT', value:float = 100) -> None:
         # add balance
         user = self.find_user(user_name, password)
         if not user:
             return None
         self.cursor.execute("INSERT INTO balance (user_id, crypto_id, value) VALUES (%s, %s, %s);", (user['id'], crypto_id, value))
         self.save_changes()
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
         
-
-    def add_crypto(self, user_name:str, password:str, crypto_name:str, short_name:str) -> None:
-        # add new cryptocurrency (only for superuser)
-        if self.find_user(user_name, password)['superuser']:
-            self.cursor.execute("INSERT INTO cryptocurrencies (name, short_name) VALUES (%s, %s);", (crypto_name, short_name))
-            self.save_changes()
-    
-    
-    def add_user(self, username:str, password:str) -> None:
-        # register
-        self.cursor.execute("INSERT INTO users (username, password) VALUES(%s, %s);", (username, password))
-        self.save_changes()
-       
-       
+        
+    # - - - - - - - - - - - - - - - - - - SELL/BUY  FUNCS - - - - - - - - - - - - - - - - - - - - - - - - - -
     def sell(self, user_id:int, first_crupto_id:int, second_crupto_id:int, price:float, value:float) -> None:
         # sell first_crupto to second_crupto
         
@@ -122,11 +136,7 @@ class Connect():
                                     (second_crypto_balance_after, user_id, second_crupto_id))
             
             self.save_changes()
-        
-    
-    def save_changes(self) -> None:
-        # just commit
-        self.conn.commit()
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     
     
     def __dell__(self) -> None:
